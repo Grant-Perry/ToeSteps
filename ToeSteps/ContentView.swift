c1e19cde543204aa0f00d7bfd7eed075229df16f
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2024-06-04 13:44:54 -0400

    Initial Commit

---
+++ ToeSteps/ContentView.swift
@@ -0,0 +1,26 @@
+//   ContentView.swift
+//   ToeSteps
+//
+//   Created by: Grant Perry on 6/4/24 at 1:44 PM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToeSteps/ToeStepsApp.swift
@@ -0,0 +1,19 @@
+//   ToeStepsApp.swift
+//   ToeSteps
+//
+//   Created by: Grant Perry on 6/4/24 at 1:44 PM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+@main
+struct ToeStepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
